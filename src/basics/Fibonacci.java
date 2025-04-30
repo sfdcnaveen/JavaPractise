d0005565b60ffaef4bf8baac14c1031212477435
Naveen Kumar Pasupuleti <pasupulatink@gmail.com>
2025-04-30 18:10:56 +0530

    Refactor code structure and remove redundant changes

--- src/basics/Fibonacci.java
+++ src/basics/Fibonacci.java
@@ -7,16 +7,15 @@ public class Fibonacci {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
-        int a=0,b=1, c;
-        System.out.print(a+" "+b+" ");
-        for(int i=1; i<=n; i++)
-        {
-            c = a+b;
-            System.out.print(c+" ");
-            a=b;
-            b=c;
+        int a = 0, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 1; i <= n; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
         }
         sc.close();
 
-    }    
+    }
 }
--- src/basics/Palindrome.java
+++ src/basics/Palindrome.java
@@ -7,17 +7,16 @@ public class Palindrome {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         int n = sc.nextInt();
-        int r,sum=0,temp;
+        int r, sum = 0, temp;
 
         temp = n;
 
-        while(n>0)
-        {
-            r=n%10;
-            sum = (sum*10)+r;
-            n=n/10;
+        while (n > 0) {
+            r = n % 10;
+            sum = (sum * 10) + r;
+            n = n / 10;
         }
-        if(temp == sum)
+        if (temp == sum)
             System.out.println("Palindrome number");
         else
             System.out.println("Not a palindrome");
